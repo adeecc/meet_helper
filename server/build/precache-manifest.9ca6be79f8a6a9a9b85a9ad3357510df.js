d1f6d4aa5a7fa58092c4ac59931dc9849957601b
Aditya Chopra <adi.chopra108@gmail.com>
2020-03-22 19:58:54 +0530

    Removed Build from Gitignore

---
+++ server/build/precache-manifest.9ca6be79f8a6a9a9b85a9ad3357510df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c940d73c6b5864cabd9995a06bed78b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5273e3f273b593723d51",
+    "url": "/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "66ffc6bdb1005c272cc0",
+    "url": "/static/css/main.de3a61fb.chunk.css"
+  },
+  {
+    "revision": "5273e3f273b593723d51",
+    "url": "/static/js/2.a907e08b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a907e08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ffc6bdb1005c272cc0",
+    "url": "/static/js/main.de186850.chunk.js"
+  },
+  {
+    "revision": "dbdddd9146d336bf79bf",
+    "url": "/static/js/runtime-main.8227d3ea.js"
+  }
+]);
