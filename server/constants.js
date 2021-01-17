bc178c475e2dfe715cfe987ef371d309fc6364d2
adichopra11 <adi.chopra108@gmail.com>
2021-01-17 19:11:55 +0530

    Fixed Security Issue

--- server/constants.js
+++ server/constants.js
@@ -1,9 +1,12 @@
 ﻿const CONSTANTS = {};
 CONSTANTS.ENDPOINT = {};
 
+let username = process.env.MONGO_USER
+let password = process.env.MONGO_PASS
+
 CONSTANTS.PORT = process.env.PORT || "3001";
 CONSTANTS.ENDPOINT.LIST = "/list";
-CONSTANTS.DB_URL = "mongodb+srv://admin:admin@cluster0-xaizu.gcp.mongodb.net/meet_help?retryWrites=true&w=majority" || process.env.MONGODB_URI;
+CONSTANTS.DB_URL = process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0-xaizu.gcp.mongodb.net/meet_help?retryWrites=true&w=majority";
 
 
 module.exports = CONSTANTS;
